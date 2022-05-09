dfe0fecbe548b1464c0dfc1e423d1225d60f70b9
Cory Quirk <coryjquirk@gmail.com>
2022-05-08 22:50:11 -0500

    flightGroupService: stayRange validation

--- flightGroupService.py
+++ flightGroupService.py
@@ -13,9 +13,18 @@ invalid_em3 = 'bad@website'
 invalid_em4 = '@email.net'
 invalid_em5 = '@email'
 invalid_em6 = 'alsobad@.net'
-# list
+# list of strings
 testcase_emails = [valid_em1, valid_em2, valid_em3, invalid_em1, invalid_em2,
                   invalid_em3, invalid_em4, invalid_em5, invalid_em6]
+
+valid_stayRange1 = (1, 2)
+valid_stayRange2 = (5, 10)
+valid_stayRange3 = (29, 30)
+invalid_stayRange1 = (0, 10)
+invalid_stayRange2 = (12, 9)
+invalid_stayRange3 = (20, 0)
+# list of tuples
+testcase_stayRanges = [valid_stayRange1, valid_stayRange2, valid_stayRange3, invalid_stayRange1, invalid_stayRange2, invalid_stayRange3]
 
 def em_isvalid(email):
 
@@ -23,6 +32,19 @@ def em_isvalid(email):
         return True
     else:
         return False
+
+
+def stayRange_isvalid(stayRange):
+
+    # stayRange is a tuple
+    if (stayRange[0] > stayRange [1]):
+        print("Second value must be greater than first value")
+        return False
+    elif ((stayRange[0] or stayRange[1]) <= 0):
+        print("Values must be greater than 0")
+        return False
+    else:
+        return True
 
 if __name__ == '__main__':
 
@@ -35,4 +57,13 @@ if __name__ == '__main__':
 
         elif (em_isvalid(testcase_emails[i])) is False:
             print("Invalid email: " + testcase_emails[i])
+        i += 1
+
+    i = 0
+    length = len(testcase_stayRanges)
+    while i < length:
+        if (stayRange_isvalid(testcase_stayRanges[i])) is False:
+            print('Invalid stayRange: {0}'.format(testcase_stayRanges[i]))
+        elif (stayRange_isvalid(testcase_stayRanges[i])) is True:
+            print('Valid stayRange: {0}'.format(testcase_stayRanges[i]))
         i += 1
